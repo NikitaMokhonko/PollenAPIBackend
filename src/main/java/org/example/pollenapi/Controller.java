a8a6e97957438faf0ac460e25828102ae45aef15
Philip Mokhonko <nikitamokhonko@gmail.com>
2025-05-16 14:10:09 +0200

    Added @CrossOrigin to GetMapping

--- src/main/java/org/example/pollenapi/Controller.java
+++ src/main/java/org/example/pollenapi/Controller.java
@@ -1,6 +1,7 @@
 package org.example.pollenapi;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class Controller {
     }
 
     @GetMapping
+    @CrossOrigin
     public Flux<Dto> getPollen() {
        return pollenService.getPollen();
     }
